1e4a21fbf31efc517b4cff2decb1a76bb198b185
Kai Painton <kpainton@D-173-250-177-243.dhcp4.washington.edu>
2018-04-09 16:29:53 -0700

    finished most of ex3

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -2,40 +2,40 @@
 
 # Load R's "USPersonalExpenditure" dataset using the `data()` function
 # This will produce a data frame called `USPersonalExpenditure`
-
+data("USPersonalExpenditure")
 
 # The variable `USPersonalExpenditure` is now accessible to you. Unfortunately,
 # it's not a data frame (it's actually what is called a matrix)
 # Test this using the `is.data.frame()` function
-
+is.data.frame("USPersonalExpenditure")
 
 # Luckily, you can pass the USPersonalExpenditure variable as an argument to the
 # `data.frame()` function to convert it a data farm. Do this, storing the
 # result in a new variable
-
+us_data <- data.frame(USPersonalExpenditure)
 
 # What are the column names of your dataframe?
-
+colnames(us_data)
 
 ## Consider: why are they so strange? Think about whether you could use a number 
 ## like 1940 with dollar notation!
 
 # What are the row names of your dataframe?
-
+rownames(us_data)
 
 # Add a column "category" to your data frame that contains the rownames
-
+us_data$category <- rownames(us_data)
 
 # How much money was spent on personal care in 1940?
-
+personal_care_1940 <- us_data["Personal Care", "X1940"]
 
 # How much money was spent on Food and Tobacco in 1960?
-
+Food_tobacco_cost <- us_data["Food and Tobacco", "X1960"]
 
 # What was the highest expenditure category in 1960?
+highest_1960 <- us_data$category[us_data$X1960 == max(us_data$X1960)]
 
-
-# Define a function `lowest_category` that takes in a year as a parameter, and
+ # Define a function `lowest_category` that takes in a year as a parameter, and
 # returns the lowest spending category of that year
 
 
