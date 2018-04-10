3fd7c150bab5711fafa78715ecb5d29cfa498489
Kai Painton <kpainton@D-173-250-177-243.dhcp4.washington.edu>
2018-04-09 17:06:07 -0700

    changed variable to grants

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -4,14 +4,14 @@
 # file into a variable called `grants` using the `read.csv()`
 # Be sure to set your working directory in RStudio, and do NOT treat strings as 
 # factors!
-data <- read.csv("data/gates_money.csv", stringsAsFactors = FALSE)
+grants <- read.csv("data/gates_money.csv", stringsAsFactors = FALSE)
 
 # Use the View function to look at the loaded data
-View(data)
+View(grants)
 
 # Create a variable `organization` that contains the `organization` column of 
 # the dataset
-organization <- data[,"organization"]
+organization <- grants[,"organization"]
 
 # Confirm that the "organization" column is a vector using the `is.vector()` 
 # function. 
@@ -21,19 +21,19 @@ is.vector("organization")
 ## Now you can ask some interesting questions about the dataset
 
 # What was the mean grant value?
-mean_grant <- mean(data$total_amount)
+mean_grant <- mean(grants$total_amount)
 
 # What was the dollar amount of the largest grant?
-largest_grant <- max(data$total_amount)
+largest_grant <- max(grants$total_amount)
 
 # What was the dollar amount of the smallest grant?
-smallest_grant <- min(data$total_amount)
+smallest_grant <- min(grants$total_amount)
 
 # Which organization received the largest grant?
-large_organization <- data[data$total_amount == largest_grant, "organization"]
+large_organization <- grants[grants$total_amount == largest_grant, "organization"]
 
 # Which organization received the smallest grant?
-smallest_organization <- data[data$total_amount == smallest_grant, "organization"]
+smallest_organization <- grants[grants$total_amount == smallest_grant, "organization"]
 
 # How many grants were awarded in 2010?
-total_awarded_2010 <- length(data$total_amount[data$start_year == "2010"])
+total_awarded_2010 <- length(grants$total_amount[grants$start_year == "2010"])
